49050f9ddb092727e2fb0fdbab73e6656b7b4f07
altannagji <altanangji@gmail.com>
2025-04-20 15:21:01 -0500

    fix: Make hqrrp more closely resemble libflame implementation.

--- project_implementation/hqrrp_blk.m
+++ project_implementation/hqrrp_blk.m
@@ -1,5 +1,6 @@
 function [ A, T, s ] = hqrrp_blk( A, T, s, b, p )
 	G = randn(b + p, size(A, 1));
+    
 	Y = G*A;
 
 	[ ATL, ATR, ...
@@ -10,7 +11,6 @@ function [ A, T, s ] = hqrrp_blk( A, T, s, b, p )
 	  TB ] = FLA_Part_2x1(T, 0, 'FLA_TOP');
 
 	[ sL, sR ] = FLA_Part_1x2(s, 0, 'FLA_LEFT');
-
 	[ YL, YR ] = FLA_Part_1x2(Y, 0, 'FLA_LEFT');
 	[ GL, GR ] = FLA_Part_1x2(G, 0, 'FLA_LEFT');
 
@@ -29,36 +29,52 @@ function [ A, T, s ] = hqrrp_blk( A, T, s, b, p )
 		[ s0, s1, s2 ] = FLA_Repart_1x2_to_1x3( sL, sR, b, 'FLA_RIGHT' );
 		[ Y0, Y1, Y2 ] = FLA_Repart_1x2_to_1x3( YL, YR, b, 'FLA_RIGHT' );
 		[ G0, G1, G2 ] = FLA_Repart_1x2_to_1x3( GL, GR, b, 'FLA_RIGHT' );
+        [~, ~, s_pivots] = hqrp_unb_flame(YR, -1, -1, b);
 
-		s1 = DeterminePivots(Y1, Y2, b); %s1 is 1xb
+
+
 		%Swap according to pivot selection
-		[ A01, A02 ] = SwapCols(s1, A01, A02);
-		[ A11, A12 ] = SwapCols(s1, A11, A12);
-		[ A21, A22 ] = SwapCols(s1, A21, A22);
-		
+		[ A01, A02 ] = SwapCols(s_pivots, A01, A02);
+		[ A11, A12 ] = SwapCols(s_pivots, A11, A12);
+		[ A21, A22 ] = SwapCols(s_pivots, A21, A22);
+        [ Y1, Y2 ] = SwapCols(s_pivots, Y1, Y2);
+		[s1, s2] = SwapCols(s_pivots, s1, s2);
 		%Perform HQRP with pivoted columns. 
-		A_11_m = size(A11, 1);
 
-		[A_out, T1, s1_prime] = hqrp_unb_flame([A11; A21], T1, b);
+		[A_out, T1, s1_prime] = hqrp_unb_flame([A11; A21], T1, -1, -1);
+        
+		A11 = A_out(1:b, :);
+		A21 = A_out(b + 1:end, :);
 
-		A11 = A_out(1:A_11_m, :);
-		A21 = A_out(A_11_m + 1:end, :);
+        %Apply permutations to A01 and Y1
  		[A01, ~] = SwapCols(s1_prime, A01, []);
-		s1 = SwapCols(s1_prime, s1, []);
+        [Y1, ~] = SwapCols(s1_prime, Y1, []);
+		[s1, ~] = SwapCols(s1_prime, s1, []);
+        
 
 		%Update the rest of the matrix.
 		U11 = tril(A11, -1) + eye(size(A11, 1));
 		U21 = A21;
 		R12 = A12;
 		T11 = triu(T1(:, 1:b));
-		W12 = T11' \ (A11' * A12 + A21' * A22);
+        disp(["U11'", size(U11')])
+        disp(["A12", size(A12)])
+        disp(["U21'", size(U21')])
+        disp(["A22", size(A22)])
+		W12 = inv(T11') * (U11' * A12 + U21' * A22);
 		A12 = A12 - U11 * W12;
 		A22 = A22 - U21 * W12;
 
 		%Update Y and G matrices.
 		if size(Y2, 2) > 0
-			[Y1, Y2] = SwapCols(s1, Y1, Y2);	
-			[G1, G2] = SwapCols(s1, G1, G2);
+           % temp = G1 * U11;
+           % temp = temp + G2 * U21;
+           % temp = temp * inv(T11);
+           % temp = temp * U11';
+           % temp = temp * R12;
+           % temp = G1 - temp;
+           % Y2 = Y2 - temp;
+            
 			Y2 = Y2 - (G1 - ((G1 * U11 + G2 * U21) * inv(T11)) * U11') * R12;
 		end
 
@@ -67,13 +83,11 @@ function [ A, T, s ] = hqrrp_blk( A, T, s, b, p )
 		  ABL, ABR ] = FLA_Cont_with_3x3_to_2x2( A00, A01, A02, ...
 												A10, A11, A12, ...
 												A20, A21, A22, ...
-												'FLA_TR' );
+												'FLA_TL' );
 
 		[ TT, ... 
 		  TB ] = FLA_Cont_with_3x1_to_2x1( T0, T1, T2, 'FLA_TOP' );
-
 		[ sL, sR ] = FLA_Cont_with_1x3_to_1x2( s0, s1, s2, 'FLA_LEFT' );
-
 		[ YL, YR ] = FLA_Cont_with_1x3_to_1x2( Y0, Y1, Y2, 'FLA_LEFT' );
 		[ GL, GR ] = FLA_Cont_with_1x3_to_1x2( G0, G1, G2, 'FLA_LEFT' );
 		
@@ -86,16 +100,12 @@ function [ A, T, s ] = hqrrp_blk( A, T, s, b, p )
 end
 
 
-function [ s1 ] = DeterminePivots(Y1, Y2, b)
-	[~, ~, s ] = hqrp_unb_flame(cat(2, Y1, Y2), -1, b);
-	s1 = s( 1 : b );
-end
 
-function [ AL, AR ] = SwapCols( s1, AL, AR )
+function [ AL, AR ] = SwapCols( perm, AL, AR )
+    % len(perm) = col(AL) + col(AR)
 	% Permutes the columns of A01, A02, A11, A12, A21, A22 according to s1
 	AL_n = size(AL, 2);
 	A_temp = [AL, AR];
-	perm = [s1, setdiff(1:size(A_temp, 2), s1)];
     A_temp = A_temp(:, perm);	
 	AL = A_temp(:, 1 : AL_n);
 	AR = A_temp(:, AL_n + 1 : end);
