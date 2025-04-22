a9a562e378976d7f330d08f9570ede3f2e75c5cf
Anshul Moondra <moondra.anshul@gmail.com>
2025-04-22 00:21:08 -0500

    change to error

--- paqr matlab source/src/paqr_test.m
+++ paqr matlab source/src/paqr_test.m
@@ -3,8 +3,8 @@ clc;
 clear;
 
 % Parameters
-m = 6;  % Number of rows
-n = 4;  % Number of columns
+m = 100;  % Number of rows
+n = 50;  % Number of columns
 
 % Generate random matrix A
 A = randn(m, n);
@@ -27,14 +27,10 @@ Q = eye(m) - V * T * V';
 Q = Q(:, ~dead_cols);
 
 
-% Reconstruct A from Q and R (ignore zero columns due to dead_cols)
-% R_clean = R(:, ~dead_cols);  % Remove dead columns from R
 A_hat = Q * R;
 
-% Truncate A to match A_hat's width
-% A_reduced = A(:, ~dead_cols);
 
 % Compute error
-error = norm(A_hat - A, 'fro');
+error = norm(A_hat - A, 'fro') / norm(A, 'fro');
 
 fprintf('Reconstruction error (Frobenius norm): %e\n', error);
